0889ffa2b05b127148873a3c7e8b056bbbdd3605
parevalo92 <85068855+parevalo92@users.noreply.github.com>
2025-09-11 20:16:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+//Toggle mobile menu
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu whe the close button is clicked
+    menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//Close menu whe the nav link is clicked
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => menuOpenButton.click());
+    });
+
+// Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//Responsives Breakpoints
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
+
+
+//LANGUAGE SCRIPT
+
+const langButtons = document.querySelectorAll("[data-language]");
+const textToChange = document.querySelectorAll("[data-section]");
+console.log(textToChange);
+
+langButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    fetch(`../lang/${button.dataset.language}.json`)
+      .then(res => res.json())
+      .then(data => {
+        textToChange.forEach((el) => {
+          const section = el.dataset.section;
+          const value = el.dataset.value;
+
+          el.innerHTML = data[section][value];
+        })
+      })
+  })
+})
