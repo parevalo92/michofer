515935e1767203f70628e4f2ec9759312e1bcc4c
parevalo92 <85068855+parevalo92@users.noreply.github.com>
2025-09-26 20:31:48 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ console.log(textToChange);
 
 langButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    fetch(`lang/${button.dataset.language}.json`)
+    fetch(`../lang/${button.dataset.language}.json`)
       .then(res => res.json())
       .then(data => {
         textToChange.forEach((el) => {
@@ -71,6 +71,4 @@ langButtons.forEach((button) => {
         })
       })
   })
-
 })
-
